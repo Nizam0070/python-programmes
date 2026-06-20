d75017233ec3e0c25355d942c43a4df77bdfaa48
Nizam0070 <clouddatabase007@gmail.com>
2026-06-20 16:20:41 +0530

    python pogrammes

---
+++ python.py
@@ -0,0 +1,30 @@
+
+#addition
+a = 10 
+b = 10
+c= a+b
+print("the sum of two num :",c)
+
+
+A,B=2,3         #int
+a="2"           #string
+c=5             #int
+d=5.0           #float
+TXT="@"         #string
+print(2*TXT*3)
+print((a+TXT)*B)
+print(A+B*c)
+print(A*d) 
+
+
+# FOR CHECKING DATA TYPE 
+name = "nizam"
+age = 21
+price = 2.99
+old = False
+a = None
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(old))
+print(type(a))
